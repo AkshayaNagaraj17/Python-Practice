37a79b9f798dacaffddf2a0128d0853575288366
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-09-14 20:34:27 +0530

    Intro done

--- intro.py
+++ intro.py
@@ -2,10 +2,17 @@
 name="Akshaya"
 age=20
 print(f"My name is {name} my age is {age}")
+
 #typecasting
+#process of converting one data type to another
 num=23
 print(float(num))
 print(str(num))
 num1=20.5
 print(int(num1))
 print(type(num))
+
+#input from user
+tell=input("Enter your name:")
+age1=int(input("Enter your age"))
+print(f"Name: {tell} age:{age1}")
