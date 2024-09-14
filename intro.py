eb7aa23697aae417b2808323097990f42d8855d8
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-09-14 20:25:37 +0530

    First basics of python

---
+++ intro.py
@@ -0,0 +1,11 @@
+#print statement
+name="Akshaya"
+age=20
+print(f"My name is {name} my age is {age}")
+#typecasting
+num=23
+print(float(num))
+print(str(num))
+num1=20.5
+print(int(num1))
+print(type(num))
