2163cfb44ac95b03e74d3538bfc9064b6c9b5db4
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-09-20 21:36:15 +0530

    Star pyramid

--- star.py
+++ star.py
@@ -1,18 +1,23 @@
 #right-angle triangle
 row=int(input("Enter number of rows:"))
 column=int(input("enter number of colums:"))
-
+print("Right-angle triangle")
 for i in range(1,row+1):
     
     for j in range(1,i+1):
         print("*",end=" ")
     print()
-
+print()
 #inverted triangle
-
-for i in range(row+1,0,-1):
+print("Inverted triangle")
+for i in range(row,0,-1):
     for j in range(1,i+1):
-        print("*",end='')
+        print("*",end=' ')
     print()
 
-
+for i in range(row):
+    for k in range(row-i-1):
+        print(" ",end="")
+    for j in range (i+1):
+        print("*",end=' ')
+    print()
