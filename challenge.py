3d2808cade07f7ddc1473a679e9adc7152c31656
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-10-02 14:31:25 +0530

    Oct-2 python

--- challenge.py
+++ challenge.py
@@ -1,3 +1,4 @@
+#1
 prices=[1,9,2,11,1,9,2]
 min_pro=1
 max_pro=0
@@ -5,4 +6,6 @@ for price in prices:
     min_pro=min(min_pro,price)
     profit=price-min_pro
     max_pro=max(max_pro,profit)
-print(max_pro)
+print("Maximum profit: ",max_pro)
+
+#2
